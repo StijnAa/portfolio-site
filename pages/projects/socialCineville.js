d395ee02235cae914b630475bb6bba33674fc6bc
StijnAa <s.tijnaa@gmail.com>
2024-05-23 13:57:10 +0200

    update

--- pages/projects/socialCineville.js
+++ pages/projects/socialCineville.js
@@ -13,14 +13,14 @@ export default function SocialCineville() {
     <>
       <Main>
         <Card
-          title="Social Cineville"
+          title="Movie Planner"
           link="https://movie-planner.vercel.app/"
-          linkText="Go with friends - link"
+          linkText="link"
           texts={[
-            "Film Planner web app that it easy to see what movies your friends want to go to. The main feature is a timeline that shows all the current and upcoming movies.",
+            "Movie Planner is a web app that makes it easy to see what movies your friends want to go to. The main feature is a timeline that shows all the current and upcoming movies.",
             "Right next to each movie, you can see which of your friends want to go. This makes it super simple to figure out who wants to see what.",
           ]}
-          src="/img/movie-planner.png"
+          src="/img/movie-planner.jpg"
         />
       </Main>
     </>
