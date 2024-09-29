3201b9b4bc967b7c9ce28ba59b7c8d1adb51981c
StijnAa <s.tijnaa@gmail.com>
2024-09-29 11:41:44 +0200

    added ios app

---
+++ pages/projects/ios-app.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+import Card from "../../components/card.component";
+
+const Main = styled.main`
+    height: 97vh;
+    @media only screen and (max-width: ${({ theme }) => theme.tablet}) {
+        height: auto;
+    }
+`;
+
+export default function Ios() {
+    return (
+        <Main>
+            <Card
+                title="Ios App"
+                texts={[
+                    "Inspiration struck me, so I guess I have to learn SwiftUI now. Jokes aside, it's quite fun. I'm not going to pivot to becoming an iOS developer, but I really enjoy this project.",
+                    "I'm building a wireless microphone app. That's all I will say about it for now.",
+                ]}
+                src="/img/ios-app.jpg"
+            />
+        </Main>
+    );
+}
